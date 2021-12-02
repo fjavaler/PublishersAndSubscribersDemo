a93fd6404ddf7a05ee0f07d64c95354bb9dbb9b8
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-02 11:21:21 -0700

    Initial Commit

---
+++ PublishersAndSubscribersDemo/PublishersAndSubscribersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublishersAndSubscribersDemoApp.swift
+//  PublishersAndSubscribersDemo
+//
+//  Created by Frederick Javalera on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct PublishersAndSubscribersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
